8c2571bec08e796dd6d4bfbe41ab8021128ba03f
kigihan <kigihan77@gmail.com>
2019-06-06 11:20:43 +0800

    t

--- xss-test-alert.js
+++ xss-test-alert.js
@@ -1,5 +1,5 @@
-document.write ("This is remote text via xss.js located at han's github " + document.domain);
-alert ("This is remote text via xss.js located at han's github " + document.domain);
+document.write ("This js is located at han's github, and your doamin is : " + document.domain);
+alert ("This js is located at han's github, and your doamin is : " + document.domain);
 
 function img_trans(){
 	var image_trans = document.createElement("img");
@@ -12,11 +12,26 @@ function img_show(){
 	image_show.setAttribute("src", "https://i1.wp.com/www.newyorkcomputerhelp.com/wp-content/uploads/2016/02/ive-been-hacked-help.png?zoom=2.625&resize=214%2C250&ssl=1");
 	document.body.appendChild(image_show);
 }
+function img_show_top(){
+	var image_show = document.createElement("img");
+	image_show.setAttribute("src", "https://i1.wp.com/www.newyorkcomputerhelp.com/wp-content/uploads/2016/02/ive-been-hacked-help.png?zoom=2.625&resize=214%2C250&ssl=1");
+	var body = document.getElementsByTagName("body")[0];
+	body.insertBefore(image_show, body.firstChild);
+}
 function ifrm(){
 	var ifrm = document.createElement("iframe");
-	ifrm.setAttribute("src", "https://www.google.com/webhp");
+	ifrm.setAttribute("src", "https://www.ptt.cc/bbs");
 	ifrm.style.width = "100%";
 	ifrm.style.height = "1024px";
 	document.body.appendChild(ifrm);
 }
-img_show();
+function ifrm_top(){
+	var ifrm = document.createElement("iframe");
+	ifrm.setAttribute("src", "https://www.ptt.cc/bbs");
+	ifrm.style.width = "100%";
+	ifrm.style.height = "1024px";
+	var body = document.getElementsByTagName("body")[0];
+	body.insertBefore(ifrm, body.firstChild);
+}
+
+img_show_top();
